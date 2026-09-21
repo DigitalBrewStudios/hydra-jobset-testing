46406a07d89133225f5080738180c173b5915257
eveeifyeve <kbkk7250@outlook.com>
2026-09-22 03:39:13 +1000

    feat: init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.hydraJobs.aarch64-darwin = {
+        test = builtins.derivation {
+          name = "asdhaejhfdhsfdjsnf";
+          system = "aarch64-darwin";
+          builder = "/bin/sh";
+          args = [
+            "-c"
+            "echo 'Hello from a raw dshfsafas!' > $out"
+          ];
+        };
+      };
+    };
+}
